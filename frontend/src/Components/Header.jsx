67e1d8bafaeb45eb2fb2f06c8c91292434c2c298
badri-2005 <badrinarayananramkumar@gmail.com>
2026-01-26 13:30:47 +0530

    fix: update logo path in index.html and comment out logo image in Header component

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return(
         <div className="bg-black text-white p-4 flex items-center justify-between shadow-md">
             <h1 className="text-2xl font-bold">DevNotex</h1>
-            <img src={logo} alt="Logo" className="h-16 w-24 rounded-2xl" />
+            {/* <img src={logo} alt="Logo" className="h-16 w-24 rounded-2xl" /> */}
         </div>
     );
 }
